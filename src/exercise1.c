13ccaaee1151549581a32cb4278cb97233d7cedc
Дмитрий Масалитин ИП-516 <masalitin.d@gmail.com>
2025-11-04 23:33:05 +0700

    try2

--- src/exercise1.c
+++ src/exercise1.c
@@ -2,13 +2,15 @@
 
 int main(int argc, char** argv) {
     double array[10];
-    double sum = 0;
-    for(int i= 0; i<10; i++){
-        scanf("%lf", &array[i]);
+    double sum = 0.0;
+    for(int i= 0; i<10.0; i++){
+        if (scanf("%lf", &array[i])!=1){
+            return 2;
+        }
     }
     for(int i=0;i<10;i++){
         sum += array[i];
     }
-    double sa = sum/10;
+    double sa = sum/10.0;
     printf("%.2f",sa);
 }
